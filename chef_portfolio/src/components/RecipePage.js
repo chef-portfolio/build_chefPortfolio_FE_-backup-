57c6356a4886fa6cb73c96b861d42e4f3371d1e3
Addison-Stavlo <AddieStavlo@gmail.com>
2019-05-20 17:46:28 -0400

    updated recipe page

--- chef_portfolio/src/components/RecipePage.js
+++ chef_portfolio/src/components/RecipePage.js
@@ -9,14 +9,39 @@ class RecipePage extends React.Component {
     )[0]
   };
 
+  // temporary for styling purposes
+  // state = {
+  //   recipe: this.props.recipes[0]
+  // };
+
   render() {
     return (
       <Recipe>
-        <h1>{this.state.recipe.name}</h1>
-        <p>{this.state.recipe.type}</p>
-        <img src={this.state.recipe.pic} alt="delicious foods!" />
-        <p>{this.state.recipe.ingreds.length} ingredients</p>
-        <p>Recipe by: {this.state.recipe.chef.name}</p>
+        <header>
+          <h1>{this.state.recipe.name}</h1>
+
+          <div className="header-contents">
+            <div className="recipe-info">
+              <img src={this.state.recipe.pic} alt="delicious foods!" />
+              <p>
+                {this.state.recipe.ingreds.length} ingredient recipe for{" "}
+                {this.state.recipe.type}
+              </p>
+            </div>
+
+            <div className="chef-info">
+              <h3>Recipe By: {this.state.recipe.chef.name}</h3>
+              <div className="card-contents">
+                <img src={this.state.recipe.chef.pic} alt="Chef's Avatar" />
+                <div className="info-box">
+                  <h4>{this.state.recipe.chef.loc}</h4>
+                  <h4>{this.state.recipe.chef.org}</h4>
+                  <contact>{this.state.recipe.chef.contact}</contact>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
       </Recipe>
     );
   }
@@ -25,7 +50,34 @@ class RecipePage extends React.Component {
 export default withRouter(RecipePage);
 
 const Recipe = styled.section`
-  h1 {
-    margin: 0 auto;
+  header {
+    h1 {
+      margin: 0 auto;
+      font-size: 46px;
+    }
+
+    .header-contents {
+      display: flex;
+      justify-content: center;
+      flex-wrap: wrap;
+      align-items: flex-start;
+
+      .chef-info {
+        margin: 20px 0 0 20px;
+        padding: 10px;
+        border: 1px dashed darkgray;
+
+        h3 {
+          margin: 0 0 10px 0;
+        }
+        .card-contents {
+          display: flex;
+
+          .info-box {
+            padding: 10px;
+          }
+        }
+      }
+    }
   }
 `;
---
+++ chef_portfolio/src/components/chefs/ChefCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function ChefCard(props) {
+  return <Card />;
+}
+
+const Card = styled.contact``;
