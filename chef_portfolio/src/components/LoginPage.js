60a9a5d888fc0cef0ffe92fbed344b4d419207da
Addison-Stavlo <AddieStavlo@gmail.com>
2019-05-23 17:00:23 -0400

    login/register hooked up

--- chef_portfolio/src/components/LoginPage.js
+++ chef_portfolio/src/components/LoginPage.js
@@ -1,15 +1,35 @@
 import React from "react";
 import styled from "styled-components";
-
+import axios from "axios";
 import Register from "./login/Register";
+import { URL } from "../data_sources/cp_backend";
+import jwt from "jsonwebtoken";
 
 export default class LoginPage extends React.Component {
   state = {
-    signUp: false
+    signUp: false,
+    username: "",
+    password: ""
   };
 
-  logIn = ev => {
+  handleChange = ev => {
+    this.setState({
+      [ev.target.name]: ev.target.value
+    });
+  };
+
+  logIn = async ev => {
     ev.preventDefault();
+    let info = {
+      username: this.state.username,
+      password: this.state.password
+    };
+
+    let response = await axios.post(`${URL}/auth/login`, info);
+
+    let token = response.data.token;
+    localStorage.setItem("access_token", token);
+
     this.props.logIn();
   };
 
@@ -26,8 +46,17 @@ export default class LoginPage extends React.Component {
           <Login>
             <h1>Login Page</h1>
             <form onSubmit={this.logIn}>
-              <input placeholder="username" />
-              <input placeholder="password" />
+              <input
+                placeholder="username"
+                name="username"
+                onChange={this.handleChange}
+              />
+              <input
+                placeholder="password"
+                name="password"
+                onChange={this.handleChange}
+                type="password"
+              />
               <button type="submit">Log In</button>
             </form>
             <button onClick={this.toggleRegister}>Sign Up</button>
--- chef_portfolio/src/components/Login_HOC.js
+++ chef_portfolio/src/components/Login_HOC.js
@@ -1,5 +1,6 @@
 import React from "react";
 import LoginPage from "./LoginPage";
+import jwt from "jsonwebtoken";
 
 const Login_HOC = App =>
   class extends React.Component {
@@ -7,12 +8,27 @@ const Login_HOC = App =>
       isLoggedIn: false
     };
 
+    componentDidMount() {
+      let token = localStorage.getItem("access_token");
+
+      if (token) {
+        let decoded = jwt.decode(token);
+
+        if (Date.now() / 1000 < decoded.exp) {
+          this.logIn();
+        } else {
+          localStorage.removeItem("access_token");
+        }
+      }
+    }
+
     logIn = ev => {
       this.setState({ isLoggedIn: true });
     };
 
     logOut = () => {
       this.setState({ isLoggedIn: false });
+      localStorage.removeItem("access_token");
     };
 
     render() {
